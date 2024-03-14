f76108ee6186a14249dec1f6f2959289336de844
Ricardo Gonzalez <ricardo_glez_14@icloud.com>
2024-03-14 10:13:06 -0600

    Fixes

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-num = 1769410
-if num % 2 == 0:
-    print("El número es par")
-else:
-    print("El número es impar")
+def even_or_odd(n : int) -> str:
+    
+    return "eovdedn"[n % 2::2]
+
+
+print(even_or_odd(1))
+
+print(even_or_odd(20))
